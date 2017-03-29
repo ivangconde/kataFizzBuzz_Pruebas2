894e44de2e2617c1af8d9d7bad7ea7560adf5040
Ivan Gomez <ivangconde@edu.xunta.es>
2017-03-29 13:16:23 +0200

    test multiples fizz

--- kataFizzBuzz_Pruebas2/src/katafizzbuzz_pruebas2/FizzBuzz.java
+++ kataFizzBuzz_Pruebas2/src/katafizzbuzz_pruebas2/FizzBuzz.java
@@ -14,6 +14,7 @@ public class FizzBuzz {
     public String getFizzBuzz(int n) {
 
         String result=String.valueOf(n);
+        if (result.indexOf("3")!=result.lastIndexOf("3")) result="FizzFizz";
         if (result.contains("3") && result.contains("5")) result="FizzBuzz";
         if (n%3==0 || result.contains("3")) result="Fizz";
         if (n%5==0 || result.contains("5")) result="Buzz";
